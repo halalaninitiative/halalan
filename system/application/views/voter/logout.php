de9952a1d41b4d4113e80851c33dca3e291349b3
Waldemar Bautista <waldemarbautista@gmail.com>
2007-11-10 14:28:57 +0000

    updated images

--- system/application/views/voter/logout.php
+++ system/application/views/voter/logout.php
@@ -1,8 +1,8 @@
 <div class="menu">
 	<div id="left_menu">
 		<ul>
-			<li><img src="<?= base_url(); ?>public/images/apply.png" alt="voter" /> VOTE</li>
-			<li><img src="<?= base_url(); ?>public/images/apply.png" alt="next" /> CONFIRM VOTE</li>
+			<li><img src="<?= base_url(); ?>public/images/ok.png" alt="voter" /> VOTE</li>
+			<li><img src="<?= base_url(); ?>public/images/ok.png" alt="next" /> CONFIRM VOTE</li>
 			<li class="active"><img src="<?= base_url(); ?>public/images/user.png" alt="next" /> LOG OUT</li>
 		</ul>
 	</div>
