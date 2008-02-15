767f986e9876d30749327b4aac0dc920c417ea2e
Darwin Bautista <daruuin@gmail.com>
2008-02-15 16:36:11 +0000

    Implemented Issue 9.

--- system/application/errors/error_db.php
+++ system/application/errors/error_db.php
@@ -29,6 +29,15 @@ margin: 			0 0 4px 0;
 	<div id="content">
 		<h1><?php echo $heading; ?></h1>
 		<?php echo $message; ?>
+		<p>Please make sure that Halalan was installed properly.</p>
+		<?php
+		if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on')
+			$protocol = 'https';
+		else
+			$protocol = 'http';
+		$base_url = trim($protocol . "://$_SERVER[HTTP_HOST]" . dirname($_SERVER['SCRIPT_NAME']), '/') . '/';
+		?>
+		<p>If you haven't installed Halalan yet, please <a href="<?php echo $base_url; ?>install">install</a> it now.</p>
 	</div>
 </body>
 </html>
