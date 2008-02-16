b458dbcea756160aef48897608bbf0020dde4db4
Darwin Bautista <daruuin@gmail.com>
2008-02-16 05:40:35 +0000

    Removed setCookie and getCookie functions. Used jquery.cookie instead.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -84,7 +84,7 @@ function saveState() {
 				checkboxes.push(i);
 		}
 	}
-	setCookie('halalan_cookie', checkboxes.toString(), 1);
+	$.cookie('halalan_cookie', checkboxes.toString(), {expires: 1, path: '/'});
 }
 function restoreState(checkboxes) {
 	checkboxes = checkboxes.split(",");
@@ -102,29 +102,9 @@ function in_array(needle, haystack) {
 	return false;
 }
 function checkCookie() {
-	var checkboxes = getCookie('halalan_cookie');
+	var checkboxes = $.cookie('halalan_cookie');
 	if(checkboxes != null && checkboxes != "")
 		restoreState(checkboxes);
-}
-
-function setCookie(c_name,value,expiredays) {
-	var exdate=new Date();
-	exdate.setDate(exdate.getDate()+expiredays);
-	document.cookie = c_name + "=" + escape(value) + ((expiredays==null) ? "" : ";expires=" + exdate.toGMTString()) + ";path=/";
-}
-
-function getCookie(c_name) {
-	if(document.cookie.length > 0) {
-		var c_start = document.cookie.indexOf(c_name + "=");
-		if(c_start != -1) {
-			c_start = c_start + c_name.length + 1;
-			c_end = document.cookie.indexOf(";", c_start);
-			if(c_end == -1)
-				c_end = document.cookie.length;
-			return unescape(document.cookie.substring(c_start,c_end));
-		}
-	}
-	return "";
 }
 
 /* jQuery code */
--- system/application/views/admin.php
+++ system/application/views/admin.php
@@ -12,6 +12,7 @@
   <script type="text/javascript" src="<?= base_url(); ?>public/javascripts/domTT/domTT.js"></script>
   <script type="text/javascript" src="<?= base_url(); ?>public/javascripts/domTT/domTT_drag.js"></script>
   <script type="text/javascript" src="<?= base_url(); ?>public/javascripts/jquery.js"></script>
+  <script type="text/javascript" src="<?= base_url(); ?>public/javascripts/jquery.cookie.js"></script>
   <script type="text/javascript" src="<?= base_url(); ?>public/javascripts/main.js"></script>
 </head>
 <body>
