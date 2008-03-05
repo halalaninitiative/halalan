2e091968b76310f952b411793a08d75afec513ab
Waldemar Bautista <waldemarbautista@gmail.com>
2008-03-05 13:07:48 +0000

    fixed base_url problem as filed in issue 41

--- system/application/config/config.php
+++ system/application/config/config.php
@@ -15,7 +15,7 @@ if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on')
 	$protocol = 'https';
 else
 	$protocol = 'http';
-$base_url = trim($protocol . "://$_SERVER[HTTP_HOST]" . dirname($_SERVER['SCRIPT_NAME']), '/') . '/';
+$base_url = $protocol . "://$_SERVER[HTTP_HOST]" . substr($_SERVER['SCRIPT_NAME'], 0, strrpos($_SERVER['SCRIPT_NAME'], '/')) . '/';
 $config['base_url']	= $base_url;
 
 /*
