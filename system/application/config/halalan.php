7bd9cf4d99f54bf1c80fd3b6ad9f08e1be269d4f
Waldemar Bautista <waldemarbautista@gmail.com>
2008-06-11 11:02:31 +0000

    updated halalan config file

--- system/application/config/halalan.php
+++ system/application/config/halalan.php
@@ -1,21 +1,21 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['language'] = 'english';
-
-$config['halalan']['name'] = "University Student Council Election";
-$config['halalan']['pin'] = FALSE;
+// don't change if you already entered some data
+$config['halalan']['name'] = "Election Name";
+$config['halalan']['pin'] = TRUE;
 $config['halalan']['password_pin_generation'] = "web";
 $config['halalan']['password_pin_characters'] = "alnum";
-$config['halalan']['password_length'] = 6;
-$config['halalan']['pin_length'] = 6;
+$config['halalan']['password_length'] = 4;
+$config['halalan']['pin_length'] = 4;
 $config['halalan']['captcha'] = TRUE;
-$config['halalan']['captcha_length'] = 5;
+$config['halalan']['captcha_length'] = 4;
 $config['halalan']['show_candidate_details'] = TRUE;
 $config['halalan']['random_order'] = TRUE;
-$config['halalan']['generate_image_trail'] = TRUE;
-$config['halalan']['image_trail_path'] = '/home/httpd/html/w/';
+$config['halalan']['generate_image_trail'] = FALSE;
+$config['halalan']['image_trail_path'] = "/var/www/html/w/";
 
-$config['halalan']['v'] = FALSE;
-$config['base_url'] = 'http://localhost/~waldemar/halalan/';
+$config['base_url'] = "http://localhost/~waldemar/halalan/";
+$config['language'] = "english";
+$config['encryption_key'] = "FIJDtfzhUllTcd41zC4a6VwtMaQlPEOl";
 
 ?>
