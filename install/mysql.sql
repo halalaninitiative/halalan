efa4dab1a079e1b95ed594222159970b13f6f563
Darwin Bautista <daruuin@gmail.com>
2011-02-21 11:07:34 +0800

    Save the session data to the database by default

--- install/mysql.sql
+++ install/mysql.sql
@@ -113,3 +113,12 @@ CREATE TABLE random_orders (
     random_order text NOT NULL,
     PRIMARY KEY  (voter_id)
 );
+
+CREATE TABLE sessions (
+    session_id varchar(40) DEFAULT '0' NOT NULL,
+    ip_address varchar(16) DEFAULT '0' NOT NULL,
+    user_agent varchar(50) NOT NULL,
+    last_activity int(10) unsigned DEFAULT 0 NOT NULL,
+    user_data text NOT NULL,
+    PRIMARY KEY (session_id)
+);
