b0e3f51f73cdd89367cb535a7329043f6a52e89e
Waldemar Bautista <waldemarbautista@gmail.com>
2015-03-15 14:26:19 +0800

    Use the mysqli driver

--- application/config/database.php
+++ application/config/database.php
@@ -57,7 +57,7 @@ $active_record = TRUE;
 $db['default']['hostname'] = "localhost";
 $db['default']['username'] = "root";
 $db['default']['password'] = "";
-$db['default']['database'] = "hololon";
+$db['default']['database'] = "halalan";
 
 /*
 |===============================================================
@@ -65,7 +65,7 @@ $db['default']['database'] = "hololon";
 |===============================================================
 */
 
-$db['default']['dbdriver'] = 'mysql';
+$db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
